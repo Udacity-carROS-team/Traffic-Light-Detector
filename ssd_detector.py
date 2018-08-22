54d56f6646d9da25e84aaa4b37c290db3853a27c
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-08-22 10:59:18 +0800

    first draft working

--- ssd_detector.py
+++ ssd_detector.py
@@ -1,1 +1,121 @@
-GRAPH_FILE = './frozen_inference_graph_pb'
+#!/usr/bin/env python
+
+import numpy as np
+import tensorflow as tf
+import cv2
+# import matplotlib.pyplot as plt
+import glob
+import time
+
+# if you want to use Hydrogen in Atom, uncomment the following line
+# %matplotlib inline
+
+GRAPH_FILE = './frozen_inference_graph.pb'
+BOXES_SCORE_MIN = 0.2
+
+
+def load_images(folders):
+    img_fns = []
+
+    for f in folders:
+        file_pattern_png = './' + f + '/*.png'
+        file_pattern_jpg = './' + f + '/*.jpg'
+        img_fns.extend(glob.glob(file_pattern_png))
+        img_fns.extend(glob.glob(file_pattern_jpg))
+
+    imgs = []
+
+    for fn in img_fns:
+        img = cv2.imread(fn)
+        imgs.append(img)
+
+    return imgs
+
+
+def load_graph(graph_file):
+    # Loads a frozen inference graph
+    graph = tf.Graph()
+    with graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(graph_file, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+    return graph
+
+
+def TLDetection(image, sess):
+
+    image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+    boxes, scores, classes = sess.run([detection_boxes, detection_scores, detection_classes],
+                                      feed_dict={image_tensor: image_np})
+
+    boxes = np.squeeze(boxes)
+    scores = np.squeeze(scores)
+    classes = np.squeeze(classes)
+
+    return boxes, scores, classes
+
+
+def TLBoxes(prob, boxes, scores, classes):
+    # filter boxes under minimum probability 'prob'
+    # COCO class index for TrafficLight is '10'
+    n = len(boxes)
+    idxs = []
+    for i in range(n):
+        if scores[i] >= prob and classes[i] == 10:
+            idxs.append(i)
+
+    filtered_boxes = boxes[idxs, ...]
+    filtered_scores = scores[idxs, ...]
+    filtered_classes = classes[idxs, ...]
+    return filtered_boxes, filtered_scores, filtered_classes
+
+
+def TLResizeBoxes(boxes, image_height, image_width):
+    # Resize boxes from original values (0:1) to image size
+    box_coords = np.zeros_like(boxes)
+    box_coords[:, 0] = boxes[:, 0] * image_height
+    box_coords[:, 1] = boxes[:, 1] * image_width
+    box_coords[:, 2] = boxes[:, 2] * image_height
+    box_coords[:, 3] = boxes[:, 3] * image_width
+
+    return box_coords
+
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=3):
+    # Draws bounding boxes
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, (bbox[1], bbox[0]), (bbox[3], bbox[2]), color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+
+if __name__ == '__main__':
+    detection_graph = load_graph(GRAPH_FILE)
+
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+    sess = tf.Session(graph=detection_graph)
+
+    imgs = load_images(['sample_data'])
+
+    for image in imgs:
+        gbeq_image = cv2.GaussianBlur(image, (5, 5), 0)
+        boxes, scores, classes = TLDetection(image, sess)
+        boxes, scores, classes = TLBoxes(BOXES_SCORE_MIN, boxes, scores, classes)
+
+        image_height = image.shape[0]
+        image_width = image.shape[1]
+        box_coordinates = TLResizeBoxes(boxes, image_height, image_width)
+
+        if len(boxes) != 0:
+            processed_image = draw_boxes(image, box_coordinates, color=(0, 0, 255), thick=3)
+            cv2.imwrite('./processed_data/file_{}.png'.format(time.time()), processed_image)
